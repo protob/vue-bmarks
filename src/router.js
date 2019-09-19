cd93d4d3f19c1f45e119fbfa2fdc60cd78bb123b
protob <protobx@gmail.com>
2019-09-19 23:46:35 +0200

    add login/register pages

--- src/router.js
+++ src/router.js
@@ -1,18 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import Home from './views/Home.vue'
-Vue.use(Router)
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
     }
-
   ]
-})
+});
---
+++ src/views/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="h-full">
+    <top-nav />
+
+    <div class="form-wrap flex justify-center">
+      <div class="bg-gray-200 shadow-md rounded form-panel mt-16">
+        <div
+          class="form-head bg-blue-700 px-8 py-4 text-white font-bold text-center"
+        >
+          <h1 class="capitalize">Register Form</h1>
+        </div>
+
+        <div class="px-8 pt-6 pb-8 mb-4">
+          <RegisterForm :formid="'registerForm'" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import topNav from "@/components/organisms/topNav.vue";
+
+import FormBuilder from "@/builders/FormBuilder";
+import FormDirector from "@/builders/FormDirector";
+export default {
+  name: "Home",
+  components: {
+    topNav,
+    RegisterForm: new FormDirector(new FormBuilder()).makeRegisterForm()
+  }
+};
+</script>
